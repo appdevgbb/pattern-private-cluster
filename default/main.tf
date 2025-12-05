6d2ffecfe2316f0969c0f5287270ceee943313f2
Your Name <your_email@example.com>
2025-12-05 13:24:54 -0700

    feat: add virtual network and subnets for AKS integration with user-assigned identity

--- default/main.tf
+++ default/main.tf
@@ -36,6 +36,48 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${var.cluster_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
+resource "azurerm_subnet" "api_server_subnet" {
+  name                 = "api-server-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.1.2.0/24"]
+  delegation {
+    name = "aks-delegation"
+    service_delegation {
+      name = "Microsoft.ContainerService/managedClusters"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
+
+resource "azurerm_user_assigned_identity" "aks_identity" {
+  name                = "aks-identity-${var.cluster_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_role_assignment" "network_contributor" {
+  scope                = azurerm_virtual_network.vnet.id
+  role_definition_name = "Network Contributor"
+  principal_id         = azurerm_user_assigned_identity.aks_identity.principal_id
+}
+
 resource "azurerm_kubernetes_cluster" "private_aks" {
   name                = var.cluster_name
   location            = azurerm_resource_group.rg.location
@@ -43,16 +85,24 @@ resource "azurerm_kubernetes_cluster" "private_aks" {
   dns_prefix          = "pvt-example"
 
   default_node_pool {
-    name       = "system"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
+    name           = "system"
+    node_count     = 1
+    vm_size        = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.aks_subnet.id
   }
 
   identity {
-    type = "SystemAssigned"
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.aks_identity.id]
   }
 
-  private_cluster_enabled = true
+  private_cluster_enabled             = true
+  private_cluster_public_fqdn_enabled = false
+
+  api_server_access_profile {
+    virtual_network_integration_enabled = true
+    subnet_id                           = azurerm_subnet.api_server_subnet.id
+  }
 }
 
 output "resource_group_name" {
