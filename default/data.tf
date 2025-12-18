7eda43dfa331b76b7c26714e2fc5997e470a759c
Diego Casati <diego.casati@gmail.com>
2025-12-18 08:52:46 -0700

    Remove Cloud Shell and AMPLS components from main branch
    Moved to feature/cloudshell-ampls branch:
    - ampls.tf - Azure Monitor Private Link Scope
    - cloudshell.tf - Cloud Shell VNet integration
    - ACI service principal data source
    
    Main branch now contains core AKS infrastructure only.

--- default/data.tf
+++ default/data.tf
@@ -4,9 +4,3 @@
 
 # Current Azure client configuration (used for role assignments)
 data "azurerm_client_config" "current" {}
-
-# Azure Container Instance service principal (well-known application ID)
-# Used for Cloud Shell VNet integration
-data "azuread_service_principal" "aci" {
-  client_id = "6bb8e274-af5d-4df2-98a3-4fd78b4cafd9"
-}
